a0940bb61f426977d60ac306850543ca902fffae
MarioSchuerewegen <47939519+MarioSchuerewegen@users.noreply.github.com>
2019-04-24 13:53:14 +0200

    deleted subdir

--- ibm-nci-datasource/dist/module.d.ts
+++
@@ -1,7 +0,0 @@
-import { IPMDatasource } from './datasource';
-import { IPMQueryCtrl } from './query_ctrl';
-import { IPMConfigCtrl } from './config_ctrl';
-declare class IPMQueryOptionsCtrl {
-    static templateUrl: string;
-}
-export { IPMDatasource as Datasource, IPMQueryCtrl as QueryCtrl, IPMQueryOptionsCtrl as QueryOptionsCtrl, IPMConfigCtrl as ConfigCtrl };
